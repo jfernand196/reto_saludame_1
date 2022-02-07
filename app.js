392d9973474ba898b444ee3070c8fca9b90e384f
jfernand <jfernand196@gmail.com>
2022-02-07 14:06:58 -0500

    update: query express

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ app.get("/", (req, res) => {
     const nombre = req.query.nombre;
 
     if(nombre){
-        res.send(`<h1> Hola ${nombre}! </h1>`)
+        res.send(`<h1>Hola ${nombre}!</h1>`)
     }
     else{
-        res.send(`<h1>Hola desconocido!.`)
+        res.send(`<h1>Hola desconocido!.</h1>`)
     }
 
    
